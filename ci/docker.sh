84640b2a18696d5837c6cb789f42aecf8763bf3b
Craig Comstock <craig.comstock@northern.tech>
2023-08-28 15:58:17 -0500

    Craigs-MacBook-Pro.local

--- ci/docker.sh
+++ ci/docker.sh
@@ -6,14 +6,17 @@ cfbs build
 # find the dir two levels up from here, home of all the repositories
 PROJECT_ROOT="$(readlink -e "$(dirname "$0")/../")"
 
+# image to use, debian for apt
+image=craigcomstock/cfengine:debian-agent
+
 # to debug things inside the container as it "will be" run
-#docker run -it -v "${PROJECT_ROOT}/out/masterfiles":/var/cfengine/inputs debian-agent bash
+#docker run -it -v "${PROJECT_ROOT}/out/masterfiles":/var/cfengine/inputs $image bash
 
 # validate policy
-docker run -v "${PROJECT_ROOT}/out/masterfiles":/var/cfengine/inputs debian-agent sh -c "cf-promises"
+docker run -v "${PROJECT_ROOT}/out/masterfiles":/var/cfengine/inputs $image sh -c "cf-promises"
 
 # run tests
 for test in $(find tests -name '*.cf'); do
-  docker run -v "${PROJECT_ROOT}/out/masterfiles":/var/cfengine/inputs debian-agent sh -c "cf-agent -KIf services/cfbs/$test"
+  docker run -v "${PROJECT_ROOT}/out/masterfiles":/var/cfengine/inputs $image sh -c "cf-agent -KIf services/cfbs/$test"
 done
 
